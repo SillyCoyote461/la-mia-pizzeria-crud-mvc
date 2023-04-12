0c15ec0af9fa659032cd253ad5e839212a80f704
Francesco C <fran461j@outlook.it>
2023-04-12 14:05:17 +0200

    Add project files.

---
+++ WebApplication1/Controllers/PizzaController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            using var _context = new PizzaContext();
+            var pizzas = _context.Pizzas.ToArray();
+            return View(pizzas);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var _context = new PizzaContext();
+            Pizza pizza = _context.Pizzas.FirstOrDefault(p => p.Id == id);
+            return View(pizza);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+
+            return View("Create");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create( Pizza pizza )
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", pizza);
+            }
+
+            Pizza record = new Pizza();
+            record.Name = pizza.Name;
+            record.Description = pizza.Description;
+            record.Price = pizza.Price;
+            record.Image = pizza.Image;
+
+            using var _context = new PizzaContext();
+
+            _context.Pizzas.Add(record);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
